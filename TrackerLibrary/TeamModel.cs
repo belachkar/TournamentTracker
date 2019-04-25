56c7ea74e3d2184a7d0373cbc94d439da50fe22e
belachkar ali <34995109+belachkar@users.noreply.github.com>
2019-04-25 02:37:50 +0100

    Initial commit

--- TrackerLibrary/TeamModel.cs
+++ TrackerLibrary/TeamModel.cs
@@ -4,9 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TrackerLibrary
-{
-    public class Class1
-    {
-    }
+namespace TrackerLibrary {
+  public class TeamModel {
+  }
 }
